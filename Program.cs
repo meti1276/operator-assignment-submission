655fddd68d2e4a968c0f748884f59c869b35c9f0
meti1276 <134025235+meti1276@users.noreply.github.com>
2023-10-10 16:52:36 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System;
+
+namespace TwentyOne
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand hotel and Casino Let start by telling me your name."
+            );
+            string playerName = Console.ReadLine();
+            Console.WWriteLine("And how much money did you bring today?");
+            int bank = Convert.Toint32(console.ReadLine());
+            console.WriteLine("Hello, (0). Would you like to join a game of 21 right now ?",
+            playername);
+            string answer = Console.ReadLine().TOLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya)"
+            {
+
+            }
+        }
+    }
+}
